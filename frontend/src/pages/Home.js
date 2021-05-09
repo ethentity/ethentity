d5a5b164f31bd1251cc626f3389885a0056f936c
Danny Chung <41158853+hachui@users.noreply.github.com>
2021-05-09 13:08:20 -0400

    connected to ethereum and integrated register method

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 import "shards-ui/dist/css/shards.min.css";
 import { Button } from "shards-react";
 import { useHistory } from "react-router-dom";
+import {ethers} from 'ethers';
 
 const Home = () => {
   const history = useHistory();
@@ -11,10 +12,19 @@ const Home = () => {
     history.push("/profile");
   }
 
+  function connectWallet(){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log(provider);
+  }
+
   return (
     <div className="container">
-      <Button theme="success" squared onClick={goToProfile}>
+      <Button theme="success" squared onClick={connectWallet}>
         Connect your wallet
+      </Button>
+      <Button theme="success" squared onClick={goToProfile}>
+        Go to profile
       </Button>
     </div>
   );
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,6 +2,7 @@ import "../styles.css";
 import React from "react";
 import { FormInput, FormRadio, Button } from "shards-react";
 import { useHistory } from "react-router-dom";
+import {ethers} from 'ethers';
 
 const Profile = () => {
   const history = useHistory();
@@ -12,6 +13,84 @@ const Profile = () => {
 
   function goToIdentities() {
     history.push("/identities");
+  }
+
+  const ethentityAddress = '0xc124B451E542C7Bbfb8ec098796a4aE3F91F8918';
+  const ethentityAbi = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "contract Member",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "Register",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "addr",
+          "type": "address"
+        }
+      ],
+      "name": "getMemberFromAddress",
+      "outputs": [
+        {
+          "internalType": "contract Member",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_firstName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_lastName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_country",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_passportNumber",
+          "type": "string"
+        }
+      ],
+      "name": "register",
+      "outputs": [
+        {
+          "internalType": "contract Member",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+  function register(){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const ethentityContract = new ethers.Contract(ethentityAddress, ethentityAbi, provider);
+    const ethentityWithSigner = ethentityContract.connect(signer);
+    const eth = ethers.utils.parseUnits("1.0", 18);
+    const tx = ethentityWithSigner.register('Danny', 'Chung', 'USA', 'AB123456');
   }
 
   return (
@@ -44,7 +123,7 @@ const Profile = () => {
         <FormInput placeholder="Passport #" />
       </div>
       <div>
-        <Button theme="success" squared>Save</Button>
+        <Button theme="success" squared onClick={register}>Save</Button>
       </div>
       <div>
         <Button theme="success" squared onClick={goToVerification}>
